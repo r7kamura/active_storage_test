9224e3699f36e62055c5ee987391138af5de4113
r7kamura <r7kamura@gmail.com>
2019-11-30 02:52:46 +0900

    Add test to produce FrozenError on save

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
+require 'stringio'
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "attach and detach" do
+    user = ::User.new
+    user.avatar.attach(
+      content_type: 'text/plain',
+      filename: 'avatar.txt',
+      io: ::StringIO.new,
+    )
+    user.avatar.detach
+    user.save!
+  end
 end
